a36d6e9736e2025fb281202a4f6b9f15075b18f6
aslan-asilon31 <aslanasilon31@gmail.com>
2025-02-28 17:01:55 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+import { Router, Route, Redirect } from 'svelte-routing';
+import About from './About.svelte'; // Pastikan path ini sesuai dengan lokasi file About.svelte
+import Contact from './Contact.svelte'; // Pastikan path ini sesuai dengan lokasi file Contact.svelte
+
+<div class="max-w-2xl px-6 py-16 mx-auto space-y-12">
+    <Router>
+        <Redirect from="/pages/visitor/about" to="/about" />
+        <Redirect from="/pages/visitor/contact" to="/contact" />
+        
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+    </Router>
+
+    <article class="space-y-8 dark:bg-gray-100 dark:text-gray-900">
+        <div class="space-y-6">
+            <h1 class="text-4xl font-bold md:tracking-tight md:text-5xl">
+                Suspendisse ut magna et ipsum sodales accumsan.
+            </h1>
+            <!-- Konten lainnya di sini -->
+        </div>
+    </article>
+</div>
